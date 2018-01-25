1b8c15643ff46daee6be223e2355a3c25d23f29e
jiamin <jih426@eng.ucsd.edu>
2018-01-25 07:45:30 -0800

    node.js updated

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -2,9 +2,14 @@
 /*
  * GET home page.
  */
+exports.view = function(req, res) {
+	var nameToShow = req.params.userName;
+	if(nameToShow === undefined) {
+		nameToShow = 'World';
+	}
+	console.log("Name is " + nameToShow);
+	res.render('index', {
+	  	'name': nameToShow,
+	});
 
-exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
-  });
-};
+}
